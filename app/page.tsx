01d5ebfa5354b5232c6a9b1b75a570939545f45e
Lohan Hébert <lohanhebert7@gmail.com>
2026-02-08 11:36:46 +0100

    automatic redirect singIn page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,5 @@
-import Link from "next/link";
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Link
-        href="/hello"
-        className="rounded bg-black px-6 py-3 text-white hover:bg-gray-800"
-      >
-        Aller à la page Hello
-      </Link>
-
-      <Link
-        href="/signIn"
-        className="rounded bg-black px-6 py-3 text-white hover:bg-gray-800"
-      >
-        Aller à la page signIn
-      </Link>
-
-      <Link
-        href="/signUp"
-        className="rounded bg-black px-6 py-3 text-white hover:bg-gray-800"
-      >
-        Aller à la page signUp
-      </Link>
-    </div>
-  );
+    redirect("/signIn") // redirige automatiquement vers la page de connexion
 }
