adac80317bf5470eef6a03c27cd46634766f9e27
Lohan Hébert <lohanhebert7@gmail.com>
2026-03-27 15:17:34 +0100

    fix build

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,26 +1,17 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+"use client";
 
-export default async function Page({ searchParams }: { searchParams: Promise<{ error: string }> }) {
-  const params = await searchParams
+import { useSearchParams } from "next/navigation";
+
+export default function AuthError() {
+  const searchParams = useSearchParams();
+  const message = searchParams.get("message");
 
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <div className="flex flex-col gap-6">
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-2xl">Sorry, something went wrong.</CardTitle>
-            </CardHeader>
-            <CardContent>
-              {params?.error ? (
-                <p className="text-sm text-muted-foreground">Code error: {params.error}</p>
-              ) : (
-                <p className="text-sm text-muted-foreground">An unspecified error occurred.</p>
-              )}
-            </CardContent>
-          </Card>
-        </div>
+    <div className="flex h-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold text-red-500">Erreur</h1>
+        <p>{message || "Une erreur est survenue."}</p>
       </div>
     </div>
-  )
+  );
 }
